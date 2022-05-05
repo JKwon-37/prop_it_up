fc3642927df604007369ed5a031d9f40c82d9958
Jonathan <jonathankwon@yahoo.com>
2022-05-05 14:21:57 -0700

    created this.state, happyBday(), this.setState, and updated <p> for age.

--- prop-it-up/src/App.js
+++ prop-it-up/src/App.js
@@ -1,13 +1,39 @@
 import PersonCard from './components/prop'
 import './App.css';
+const people = [
+  {
+    firstName: "Jonathan",
+    lastName: "Kwon",
+    age: 30,
+    hair: "black"
+  },
+  {
+    firstName: "Lamar",
+    lastName: "Jackson",
+    age: 27,
+    hair: "black"
+  },
+  {
+    firstName: "Ja'marr",
+    lastName: "Chase",
+    age: 24,
+    hair: "black"
+  },
+  {
+    firstName: "Tom",
+    lastName: "Brady",
+    age: 45,
+    hair: "brown"
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName="Jonathan" lastName="Kwon" age="30" hairColor="Black"/>
-      <PersonCard firstName="Sally" lastName="Wally" age="23" hairColor="Orange"/>
-      <PersonCard firstName="Blake" lastName="Fulgham" age="32" hairColor="Brown"/>
-      <PersonCard firstName="Jin" lastName="Pham" age="25" hairColor="Blonde"/>
+      {people.map((p, i) => {
+        return (<PersonCard x={p} key={i}/>)
+      })
+    }
     </div>
   );
 }
--- prop-it-up/src/components/prop.jsx
+++ prop-it-up/src/components/prop.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        // console.log(this.props.person);
+        this.state = {
+            ...this.props.x,
+            age: this.props.x.age
+            
+        }
+        // console.log(JSON.stringify(this.props.age));
+    }
+
+    happyBday() {
+        // console.log('clicked');
+        this.setState({
+            age: this.state.age + 1
+        }, () => console.log(this.state.age))
+    }
     render() {
         return (
             <div>
-                <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
-                <p>Hair Color: {this.props.hairColor}</p>
+                {/* <p>
+                    {console.log(this.props.people.lastName)}
+                </p> */}
+                <h1>{this.state.lastName}, {this.state.firstName}</h1>
+                <p>Age: {this.state.age}
+                </p>
+                <p>Hair Color: {this.state.hair}</p>
+                <button onClick={() => this.happyBday()}>Happy Birthday!</button>
             </div>
         );
     }
